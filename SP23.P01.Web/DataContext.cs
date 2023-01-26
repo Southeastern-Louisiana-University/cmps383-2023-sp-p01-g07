db6c0e583870a9f02e71465d06491d5a6e0c26b0
Kaitlyn Hayes <kaitlyn.hayes-2@selu.edu>
2023-01-26 16:32:26 -0600

    updates
    making updates to the code

---
+++ SP23.P01.Web/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {
+
+    }
+
+};
